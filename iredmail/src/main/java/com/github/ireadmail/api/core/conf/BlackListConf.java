54d143288fabc914f3f9a619da5848a1572ce304
lianshufeng <251708339@qq.com>
2022-01-03 04:31:19 +0800

    submit

--- iredmail/src/main/java/com/github/ireadmail/api/core/conf/BlackListConf.java
+++ iredmail/src/main/java/com/github/ireadmail/api/core/conf/BlackListConf.java
@@ -6,13 +6,14 @@ import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
 import java.util.Vector;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Component
-@ConfigurationProperties(prefix = "blackList")
+@ConfigurationProperties(prefix = "blacklist")
 public class BlackListConf {
 
     private Vector<String> ip;
